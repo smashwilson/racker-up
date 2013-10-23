175172cb5bbc20b26d9de469aa6b7756084d08cb
Ash Wilson <ash.wilson@rackspace.com>
2013-10-23 18:44:18 +0000

    Enumerate a user's service catalog and active servers.

--- api1.rb
+++ api1.rb
@@ -3,10 +3,19 @@
 require 'httparty'
 require 'json'
 require 'dotenv'
+require 'pp'
 
 Dotenv.load
 
-Session = Struct.new(:token_id, :tenant_id)
+Session = Struct.new(:token_id, :tenant_id, :catalog) do
+  def auth_headers
+    { 'X-Auth-Token' => token_id }
+  end
+end
+
+CatalogEntry = Struct.new(:name, :endpoints)
+
+CloudServer = Struct.new(:name, :id)
 
 class AuthenticationEndpoint
   include HTTParty
@@ -18,11 +27,52 @@ class AuthenticationEndpoint
     body = {auth: {'RAX-KSKEY:apiKeyCredentials' => {username: username, apiKey: key}}}
     headers = {'Content-Type' => 'application/json'}
     resp = post('/tokens', headers: headers, body: body.to_json).parsed_response
+
     token_info = resp['access']['token']
-    Session.new(token_info['id'], token_info['tenant']['id'])
+    entries = resp['access']['serviceCatalog'].map do |service|
+      CatalogEntry.new(service['name'], service['endpoints'].map { |e| e['publicURL'] })
+    end
+
+    Session.new(token_info['id'], token_info['tenant']['id'], entries)
   end
 end
 
+class CloudServers
+  include HTTParty
+
+  def initialize session
+    @session = session
+    entries = @session.catalog.select { |c| c.name == 'cloudServersOpenStack' }
+    @endpoints = entries.map(&:endpoints).flatten
+  end
+
+  def list
+    @endpoints.map do |url|
+      resp = self.class.get("#{url}/servers", headers: @session.auth_headers).parsed_response
+      resp['servers'].map { |s| CloudServer.new(s['name'], s['id']) }
+    end.flatten
+  end
+end
+
+# Authenticate and report the token and tenant.
+puts ">> Authentication"
 session = AuthenticationEndpoint.login!
 puts "Token ID: #{session.token_id}"
 puts "Tenant ID: #{session.tenant_id}"
+
+# Enumerate the user's service catalog.
+puts
+puts ">> Service catalog"
+session.catalog.each do |entry|
+  puts "#{entry.name}:"
+  entry.endpoints.each do |url|
+    puts " #{url}"
+  end
+end
+
+# Enumerate the user's cloud servers (across all endpoints).
+puts
+puts ">> Active cloud servers"
+CloudServers.new(session).list.each do |s|
+  puts " Server: #{s.name} id: #{s.id}"
+end
