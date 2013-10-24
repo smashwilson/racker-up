d0eb4373b002b84d719b153f2147bf5e7466cc70
Ash Wilson <ash.wilson@rackspace.com>
2013-10-24 21:38:33 +0000

    Enumerate flavors and images.
    Launch and delete a server.

--- raw-api.rb
+++ raw-api.rb
@@ -64,6 +64,43 @@ class CloudServers
       resp['servers'].map { |s| CloudServer.new(s['name'], s['id']) }
     end.flatten
   end
+
+  def create endpoint, name, flavor, image
+    postbody = { server: { name: name, flavorRef: flavor.id.to_i, imageRef: image.id } }
+    headers = @session.auth_headers.merge('Content-Type' => 'application/json')
+    resp = HTTParty.post("#{endpoint}/servers", body: postbody.to_json, headers: headers).parsed_response
+
+    link = resp['server']['links'].find { |links| links['rel'] == 'self' }['href']
+    CloudServer.new(name, resp['server']['id'], @session, link)
+  end
+end
+
+class Flavor
+  Attrs = %i{id name ram swap vcpus disk}
+  attr_accessor *Attrs
+
+  def self.list session, endpoint
+    resp = HTTParty.get("#{endpoint}/flavors/detail", headers: session.auth_headers).parsed_response
+    resp['flavors'].map do |f|
+      Flavor.new.tap do |flavor|
+        Attrs.each { |attr| flavor.send("#{attr}=", f[attr.to_s]) }
+      end
+    end
+  end
+end
+
+class Image
+  Attrs = %i{id name}
+  attr_accessor *Attrs
+
+  def self.list session, endpoint
+    resp = HTTParty.get("#{endpoint}/images/detail", headers: session.auth_headers).parsed_response
+    resp['images'].map do |i|
+      Image.new.tap do |image|
+        Attrs.each { |attr| image.send("#{attr}=", i[attr.to_s]) }
+      end
+    end
+  end
 end
 
 # Authenticate and report the token and tenant.
@@ -75,12 +112,17 @@ puts "Tenant ID: #{session.tenant_id}"
 # Enumerate the user's service catalog.
 puts
 puts ">> Service catalog"
+dfw_endpoint = nil
 session.catalog.each do |entry|
   puts "#{entry.name}:"
   entry.endpoints.each do |url|
     puts " #{url}"
+    # Remember the DFW endpoint for cloudServersOpenStack so we can use it later.
+    dfw_endpoint = url if url =~ /dfw/ && entry.name =~ /cloudServersOpenStack/
   end
 end
+
+raise "No DFW endpoint!" unless dfw_endpoint
 
 # Enumerate the user's cloud servers (across all endpoints).
 puts
@@ -88,3 +130,43 @@ puts ">> Active cloud servers"
 CloudServers.new(session).list.each do |s|
   puts " Server: #{s.name} id: #{s.id}"
 end
+
+# List the flavors available in the DFW endpoint.
+puts
+puts ">> Flavor list"
+
+standard512 = nil
+Flavor.list(session, dfw_endpoint).each do |f|
+  puts " Flavor: #{f.id} #{f.name} CPUs: #{f.vcpus} RAM: #{f.ram} swap: #{f.swap} disk: #{f.disk}"
+
+  # Remember the one we want to create.
+  standard512 = f if f.name =~ /512MB/
+end
+
+raise "No 512MB flavor!" unless standard512
+
+# List the images available in the DFW endpoint.
+puts
+puts ">> Image list"
+
+ubuntu1310 = nil
+Image.list(session, dfw_endpoint).each do |i|
+  puts " Image: #{i.id} #{i.name}"
+  ubuntu1310 = i if i.name =~ /Ubuntu 13\.10/
+end
+
+raise "No Ubuntu 13.10 image!" unless ubuntu1310
+
+# Create a server!
+puts
+puts ">> Creating a server"
+
+server = CloudServers.new(session).create(dfw_endpoint, 'throwaway', standard512, ubuntu1310)
+
+while (prog = server.progress) != 100
+  puts "Progress: #{prog}"
+  sleep 10
+end
+
+puts ">> Deleting a server"
+server.delete
