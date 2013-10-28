55d7e2b80325bc0ec6191755e11fb8078c2292dd
Ash Wilson <ash.wilson@rackspace.com>
2013-10-28 22:41:40 +0000

    Challenge 4: Complete, but untested.

---
+++ challenge-4.rb
@@ -0,0 +1,42 @@
+# create a new A record when passed an FQDN and IP address as arguments.
+
+require_relative 'boilerplate'
+require 'optparse'
+
+fqdn, ip, email = nil, nil, nil
+opts = OptionParser.new do |opts|
+  opts.banner = "Usage: #{$0} -f DOMAINNAME -i 1.2.3.4 -e myname@email.com"
+
+  opts.on('-f', '--fqdn DOMAINNAME', 'The domain name.') do |d|
+    fqdn = d
+  end
+  opts.on('-i', '--ip', 'The IP address to register.') do |i|
+    ip = i
+  end
+  opts.on('-e', '--email', 'The email address to use as a POC.') do |e|
+    email = e
+  end
+  opts.on('-h', '--help', "You're looking at it.") do
+    puts opts
+    exit 0
+  end
+end
+
+unless fqdn && ip && email
+  puts opts
+  exit 1
+end
+
+service = log_me_in Fog::DNS
+
+puts ">> creating a zone for the address."
+zone = service.zones.create domain: fqdn, email: email
+
+puts ".. zone created with nameservers: #{zone.nameservers.join ', '}"
+
+puts ">> adding an A record to the zone."
+record = zone.records.create(
+  value: ip,
+  name: fqdn,
+  type: 'A'
+)
